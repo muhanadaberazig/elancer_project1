e12d519809c6485b7e26cd9e4c4259d5dfd73a05
Mohannad98 <nmnm8957@gmail.com>
2021-11-08 09:20:36 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/elancer_project1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.elancer_project1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Screen/lunch_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class LunchScreen extends StatefulWidget {
+  const LunchScreen({Key? key}) : super(key: key);
+
+  @override
+  _LunchScreenState createState() => _LunchScreenState();
+}
+
+class _LunchScreenState extends State<LunchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
